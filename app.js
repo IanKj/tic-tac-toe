21143f25d230797bf0d70c493f0e6644fdfe60a8
Ian Kjera <iankjera@gmail.com>
2021-05-21 21:25:43 -0400

    still needs a lot of work

--- app.js
+++ app.js
@@ -1,114 +1,286 @@
-const Gameboard = (function() {
+const Gameboard = (function () {
     const player1 = playerFactory('Player One', 'X');
     const player2 = playerFactory('Computer', 'O')
     const playerSelections = ['', '', '', '', '', '', '', '', ''];
     const divs = document.querySelectorAll('div');
-    
+
     const grid = document.querySelector('.gridContainer')
     const spaces = Array.from(grid.children);
-    function test () {
+    function test() {
         playerSelections.forEach((element, index) => {
             spaces[index] = element;
         })
-        
+
         playerSelections.forEach((element, index) => {
             divs[index].innerText = element;
-            divs[index].addEventListener('click', function() {
-                playerSelections[index] = player2.symbol;
-                divs[index].innerText = playerSelections[index];
-                if(GameControl.checkForWinner()){
-                    GameControl.clearBoard()
+            divs[index].addEventListener('click', function () {
+                if (playerSelections[index] === "") {
+                    playerSelections[index] = player1.symbol;
+                    divs[index].innerText = playerSelections[index];
+                    GameControl.checkForWinner(player1)
+
+                    GameControl.computerPlays()
+                    // if (GameControl.checkForWinner()) {
+                    //     console.log('insdie second checkforwinner')
+
+                    // }
                 }
+
             })
         })
     }
     return {
         playerSelections,
         test,
-        divs
+        divs,
+        player1,
+        player2
     }
 })();
 
 Gameboard.test()
 
 function playerFactory(name, symbol) {
-    return {name, symbol}
+    return { name, symbol }
 }
-// const players = [
-//     {
-//         player1: 'Player One',
-//         symbol: 'X'
-//     },
 
-//     {
-//         player2: 'Player Two',
-//         symbol: 'O'
-//     },
-
-//     {
-//         computer: 'Computer',
-//         symbol: 'O'
-//     },
-// ]
-
-const GameControl = (function() {
+const GameControl = (function () {
     const playerSelections = Gameboard.playerSelections
+    let winningCombos = [
+        [
+            playerSelections[0],
+            playerSelections[1],
+            playerSelections[2]
+        ],
+        [
+            playerSelections[0],
+            playerSelections[3],
+            playerSelections[6]
+        ],
+        [
+            playerSelections[0],
+            playerSelections[4],
+            playerSelections[8]
+        ],
+        [
+            playerSelections[1],
+            playerSelections[4],
+            playerSelections[7]
+        ],
+        [
+            playerSelections[2],
+            playerSelections[5],
+            playerSelections[8]
+        ],
+        [
+            playerSelections[2],
+            playerSelections[4],
+            playerSelections[6]
+        ],
+        [
+            playerSelections[3],
+            playerSelections[4],
+            playerSelections[5]
+        ],
+        [
+            playerSelections[6],
+            playerSelections[7],
+            playerSelections[8]
+        ],
 
-    function checkForWinner() {
-        if(playerSelections[0]){
-            if ((playerSelections[0] === playerSelections[1]) && 
-                (playerSelections[1] === playerSelections[2])){
-                    return true
-            } 
-            else if ((playerSelections[0] === playerSelections[3]) && 
-            (playerSelections[3] === playerSelections[6])){
-                return true
-            }
-            else if ((playerSelections[0] === playerSelections[4]) && 
-            (playerSelections[4] === playerSelections[8])){
-                return true
-            }
+    ]
+
+
+
+
+    function computerPlays() {
+        let ranNum = Math.floor(Math.random() * Gameboard.playerSelections.length)
+        if (Gameboard.playerSelections[ranNum] === '') {
+            Gameboard.playerSelections[ranNum] = Gameboard.player2.symbol;
+            Gameboard.divs[ranNum].innerText = Gameboard.player2.symbol;
+        } else {
+            computerPlays()
+            //checkForWinner(Gameboard.player2)
         }
-        if(playerSelections[1]){
-            if ((playerSelections[1] === playerSelections[4]) && 
-                (playerSelections[4] === playerSelections[7])){
-                    return true
-            } 
-        }
-        if(playerSelections[2]){
-            if ((playerSelections[2] === playerSelections[5]) && 
-                (playerSelections[5] === playerSelections[8])){
-                    return true
-            } 
-            else if ((playerSelections[2] === playerSelections[4]) && 
-            (playerSelections[4] === playerSelections[6])){
-                return true
-            }
-        } 
-        if(playerSelections[3]){
-            if ((playerSelections[3] === playerSelections[4]) && 
-                (playerSelections[4] === playerSelections[5])){
-                    return true
-            } 
-        }
-        if(playerSelections[6]){
-            if ((playerSelections[6] === playerSelections[7]) && 
-                (playerSelections[7] === playerSelections[8])){
-                    return true
-            } 
-        }  
     }
 
+    function checkForWinner(player) {
+        let winningCombos = [
+            [
+                playerSelections[0],
+                playerSelections[1],
+                playerSelections[2]
+            ],
+            [
+                playerSelections[0],
+                playerSelections[3],
+                playerSelections[6]
+            ],
+            [
+                playerSelections[0],
+                playerSelections[4],
+                playerSelections[8]
+            ],
+            [
+                playerSelections[1],
+                playerSelections[4],
+                playerSelections[7]
+            ],
+            [
+                playerSelections[2],
+                playerSelections[5],
+                playerSelections[8]
+            ],
+            [
+                playerSelections[2],
+                playerSelections[4],
+                playerSelections[6]
+            ],
+            [
+                playerSelections[3],
+                playerSelections[4],
+                playerSelections[5]
+            ],
+            [
+                playerSelections[6],
+                playerSelections[7],
+                playerSelections[8]
+            ],
+
+        ]
+        let mySwitch = false;
+        for (let combo of winningCombos) {
+            console.log(combo)
+            if (combo[0] !== '') {
+                if (combo.every(e => e === combo[0])) {
+                    mySwitch = true;
+                    console.log(mySwitch)
+                }
+            }
+            if (mySwitch === true) {
+                console.log('cheese')
+                return true
+            }
+        }
+
+
+
+
+
+    }
+
+    function showWinner() {
+
+    }
     function clearBoard() {
         Gameboard.playerSelections.forEach((element, index) => {
-                Gameboard.playerSelections[index] = '';
-                Gameboard.divs[index].innerText = playerSelections[index];
-            })
-        
+            Gameboard.playerSelections[index] = '';
+            Gameboard.divs[index].innerText = playerSelections[index];
+        })
+
     }
+
+
     return {
         checkForWinner,
         clearBoard,
-        playerSelections
+        playerSelections,
+        computerPlays
     }
 })();
+
+
+
+
+
+    // if (playerSelections[0]) {
+    //         if (
+    //             ((playerSelections[0] === playerSelections[1]) &&
+    //                 (playerSelections[1] === playerSelections[2]))
+    //             ||
+    //             ((playerSelections[0] === playerSelections[3]) &&
+    //                 (playerSelections[3] === playerSelections[6]))
+
+    //             ||
+    //             ((playerSelections[0] === playerSelections[4]) &&
+    //                 (playerSelections[4] === playerSelections[8]))
+    //         ) {
+    //             console.log('hmmm')
+    //             return true
+    //         }
+    //     }
+    //     if (playerSelections[1]) {
+    //         if ((playerSelections[1] === playerSelections[4]) &&
+    //             (playerSelections[4] === playerSelections[7])) {
+    //             return true
+    //         }
+    //     }
+    //     if (playerSelections[2]) {
+    //         if ((playerSelections[2] === playerSelections[5]) &&
+    //             (playerSelections[5] === playerSelections[8])) {
+    //             return true
+    //         }
+    //         else if ((playerSelections[2] === playerSelections[4]) &&
+    //             (playerSelections[4] === playerSelections[6])) {
+    //             return true
+    //         }
+    //     }
+    //     if (playerSelections[3]) {
+    //         if ((playerSelections[3] === playerSelections[4]) &&
+    //             (playerSelections[4] === playerSelections[5])) {
+    //             return true
+    //         }
+    //     }
+    //     if (playerSelections[6]) {
+    //         if ((playerSelections[6] === playerSelections[7]) &&
+    //             (playerSelections[7] === playerSelections[8])) {
+    //             return true
+    //         }
+    //     }
+    // }
+
+    //     if (playerSelections[0]) {
+    //         if (
+    //             ((playerSelections[0] === playerSelections[1]) &&
+    //                 (playerSelections[1] === playerSelections[2]))
+    //             ||
+    //             ((playerSelections[0] === playerSelections[3]) &&
+    //                 (playerSelections[3] === playerSelections[6]))
+
+    //             ||
+    //             ((playerSelections[0] === playerSelections[4]) &&
+    //                 (playerSelections[4] === playerSelections[8]))
+    //         ) {
+    //             console.log('hmmm')
+    //             return true
+    //         }
+    //     }
+    //     if (playerSelections[1]) {
+    //         if ((playerSelections[1] === playerSelections[4]) &&
+    //             (playerSelections[4] === playerSelections[7])) {
+    //             return true
+    //         }
+    //     }
+    //     if (playerSelections[2]) {
+    //         if ((playerSelections[2] === playerSelections[5]) &&
+    //             (playerSelections[5] === playerSelections[8])) {
+    //             return true
+    //         }
+    //         else if ((playerSelections[2] === playerSelections[4]) &&
+    //             (playerSelections[4] === playerSelections[6])) {
+    //             return true
+    //         }
+    //     }
+    //     if (playerSelections[3]) {
+    //         if ((playerSelections[3] === playerSelections[4]) &&
+    //             (playerSelections[4] === playerSelections[5])) {
+    //             return true
+    //         }
+    //     }
+    //     if (playerSelections[6]) {
+    //         if ((playerSelections[6] === playerSelections[7]) &&
+    //             (playerSelections[7] === playerSelections[8])) {
+    //             return true
+    //         }
+    //     }
+    // }
