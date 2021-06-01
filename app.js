b0bf534af128622b2d1ed58f13393e278a3ca76b
Ian Kjera <iankjera@gmail.com>
2021-05-31 21:54:06 -0400

    still functioning. added option for AI or 2player. broke AI in the process.

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const Gameboard = (function () {
-    const player1 = playerFactory('Player One', 'X');
-    const player2 = playerFactory('Computer', 'O')
+    let playingComputer = true;
+    let player1Turn = true;
+    let player1 = playerFactory('Player One', 'X');
+    let player2 = playerFactory('Computer', 'O');
+    const form = document.querySelector('form')
+    const player1Name = document.querySelector('#player1Input');
+    const player2Name = document.querySelector('#player2Input');
+    form.addEventListener('submit', formFunction)
+    function formFunction() {
+        GameControl.resetBoard();
+        player1 = playerFactory(player1Name.value.toUpperCase(), 'X');
+        player2 = playerFactory(player2Name.value, 'O');
+        playingComputer = false;
+    }
     const playerSelections = ['', '', '', '', '', '', '', '', ''];
-    const divs = document.querySelectorAll('div');
+    const divs = document.querySelectorAll('.gridContainer div');
     let winningCombos =
         [
             [0, 1, 2],
@@ -24,17 +36,30 @@ const Gameboard = (function () {
         playerSelections.forEach((element, index) => {
             divs[index].innerText = element;
             divs[index].addEventListener('click', function () {
-                if (playerSelections[index] === "") {
-                    playerSelections[index] = player1.symbol;
-                    divs[index].innerText = playerSelections[index];
-                    GameControl.checkForWinner(player1)
-                    if (!GameControl.checkForWinner(player1)) {
-                        GameControl.computerPlays()
+                if (player1Turn === true) {
+                    makeMove(player1, element, index)
+                    player1Turn = false;
+                } else {
+                    makeMove(player2, element, index)
+                    player1Turn = true;
+                }
+            })
+        })
+
+        function makeMove(player, element, index) {
+            if (playerSelections[index] === "") {
+                playerSelections[index] = player.symbol;
+                divs[index].innerText = playerSelections[index];
+                GameControl.checkForWinner(player)
+                GameControl.checkForDraw()
+                if (playingComputer === true) {
+                    if (!GameControl.checkForDraw() && !GameControl.checkForWinner(player1)) {
+                        GameControl.opponentPlays(player2);
                     }
                 }
 
-            })
-        })
+            }
+        }
     }
     return {
         playerSelections,
@@ -42,7 +67,8 @@ const Gameboard = (function () {
         divs,
         player1,
         player2,
-        winningCombos
+        winningCombos,
+        player1Turn
     }
 })();
 
@@ -54,17 +80,19 @@ function playerFactory(name, symbol) {
 
 const GameControl = (function () {
     const playerSelections = Gameboard.playerSelections
-    const grid = document.querySelector('main');
+    const grid = document.querySelector('.gridContainer');
     const resetBtn = document.querySelector('.resetBtn')
+    const results = document.querySelector('.results')
+    const gridOverlay = document.querySelector('.gridOverlay')
 
-    function computerPlays() {
+    function opponentPlays(player) {
         let ranNum = Math.floor(Math.random() * Gameboard.playerSelections.length)
         if (Gameboard.playerSelections[ranNum] === '') {
-            Gameboard.playerSelections[ranNum] = Gameboard.player2.symbol;
-            Gameboard.divs[ranNum].innerText = Gameboard.player2.symbol;
+            Gameboard.playerSelections[ranNum] = player.symbol;
+            Gameboard.divs[ranNum].innerText = player.symbol;
             checkForWinner(Gameboard.player2)
         } else {
-            computerPlays()
+            opponentPlays(player)
 
         }
     }
@@ -125,29 +153,52 @@ const GameControl = (function () {
 
                     Gameboard.divs[innerCombo].classList.add('winner')
                 }
+                results.innerText = `${player.name} wins!`
                 mySwitch = true
+                //exit loop after first winningCombo is discovered
+                return true
+            }
+            if (mySwitch === true) {
+                checkForDraw(player)
+                return
             }
         }
-        if (mySwitch === true) {
+
+
+
+    }
+
+    function checkForDraw() {
+        if ((Gameboard.playerSelections.every(el => el !== '')) && (!GameControl.checkForWinner(Gameboard.player))) {
+            grid.classList.add('clicksDisabled', 'draw');
+            gridOverlay.classList.remove('hidden');
+            gridOverlay.classList.add('z-index')
             return true
         }
     }
     resetBtn.addEventListener('click', resetBoard);
 
     function resetBoard() {
-        grid.classList.remove('clicksDisabled')
         Gameboard.playerSelections.forEach((element, index) => {
             Gameboard.playerSelections[index] = '';
             Gameboard.divs[index].innerText = '';
             Gameboard.divs[index].classList.remove('winner')
         })
+        grid.classList.remove('clicksDisabled', 'draw');
+        results.innerText = '';
+        gridOverlay.classList.add('hidden');
+        gridOverlay.classList.remove('z-index')
+        Gameboard.player1Turn = true;
+        console.log(Gameboard.player1Turn)
     }
 
 
     return {
         checkForWinner,
         playerSelections,
-        computerPlays
+        opponentPlays,
+        checkForDraw,
+        resetBoard
     }
 
 })();
